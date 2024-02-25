f9372b07bef2954119232e8313c7f696a6f412e3
Mehdi kheireddine <mahdikheireddine7@gmail.com>
2024-02-25 23:11:56 +0100

    First commit

---
+++ server.py
@@ -0,0 +1,18 @@
+import asyncio
+import websockets
+
+async def hello(websocket):
+    name = await websocket.recv()
+    print(f'Server Received: {name}')
+    greeting = f'Hello {name}!'
+
+    await websocket.send(greeting)
+    print(f'Server Sent: {greeting}')
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    print("[STARTING] server is starting .....")
+    asyncio.run(main())
